4ccd22473eb4e048bcbb83f69ff66be428aad220
Joel Kaasinen <joel.kaasinen@gmail.com>
2013-01-26 18:49:34 +0200

    Views.hs

--- Main.hs
+++ Main.hs
@@ -4,11 +4,13 @@ module Main where
 
 import Domain
 import Store
+import View
 
-import Control.Applicative (optional)
+import Control.Applicative (optional, (<$>))
 import Control.Exception
 import Control.Monad (msum, forM_)
 import Control.Monad.Trans (liftIO)
+import qualified Data.Map.Strict as M
 import Data.Text (Text)
 import Data.Text.Lazy (unpack)
 import Happstack.Server
@@ -24,8 +26,7 @@ main = bracket openDatabase
 route :: DB -> ServerPart Response
 route db = msum
   [ dir "transactions" $ page_transactions db
-  , dir "debtgraph" $ page_debtgraph db
-  , homePage ]
+  , homePage db]
 
 template :: Text -> Html -> Response
 template title body = toResponse $
@@ -36,19 +37,24 @@ template title body = toResponse $
       body
       p $ a ! href "/" $ "back home"
 
-homePage = ok $ template "Home Page" $ do
-  H.h1 "Hello!"
-  H.p $ a ! href "/transactions" $ "transactions"
-  H.p $ a ! href "/debtgraph" $ "debtgraph"
+homePage db = do
+  g <- liftIO $ viewDebtGraph db
+  let bal = unBalances $ balances g
+  ok $ template "Home Page" $ do
+    H.h1 "Balances"
+    H.ul $ forM_ (M.assocs $ g) $ \(p,status) -> H.li $ do
+      toHtml p
+      toHtml (" : " :: String)
+      toHtml (M.findWithDefault (Balance 0) p bal)
+      H.ul $ forM_ (M.assocs $ status) $ \(p',balance) -> H.li $ do
+        toHtml p'
+        toHtml (" : " :: String)
+        toHtml balance
+    H.p $ a ! href "/transactions" $ "transactions"
+    H.p $ a ! href "/debtgraph" $ "debtgraph"
 
 page_transactions :: DB -> ServerPart Response
 page_transactions db = do
   ts <- liftIO $ viewTransactions db
   ok $ template "Transactions" $ do
-    H.ul $ forM_ ts (H.li . toHtml . show)
-
-page_debtgraph :: DB -> ServerPart Response
-page_debtgraph db = do
-  g <- liftIO $ viewDebtGraph db
-  ok $ template "Debt Graph" $ do
-    H.p $ toHtml (show g)
+    H.ul $ forM_ ts (H.li . toHtml)
---
+++ View.hs
@@ -0,0 +1,33 @@
+module View where
+
+import Domain
+
+import Control.Monad (forM_)
+import Text.Blaze (ToMarkup(..))
+import Text.Blaze.Html5 (Html, (!), toHtml, toValue)
+import qualified Text.Blaze.Html5 as H
+import Text.Blaze.Html5.Attributes (class_, href)
+
+instance ToMarkup Person where
+  toMarkup (Person s) = H.span ! class_ (toValue "person") $
+                        H.a ! href (toValue $ "/persons/"++s) $
+                        toHtml s
+
+instance ToMarkup Balance where
+  toMarkup (Balance b) = H.span ! class_ (toValue "balance") $
+                         toHtml b
+
+instance ToMarkup SimpleTransaction where
+  toMarkup (SimpleTransaction payer benefitors sum descr time) =
+    H.span ! class_ (toValue "transaction") $ do
+      toHtml "Payer: "
+      H.span ! class_ (toValue "payer") $ toHtml payer
+      toHtml " Benefitors: "
+      forM_ benefitors (\b -> H.span ! class_ (toValue "benefitor") $
+                              toHtml b >> toHtml ", ")
+      toHtml " Sum: "
+      toHtml sum
+      toHtml " "
+      toHtml (show descr)
+      toHtml " @ "
+      toHtml (show time)
