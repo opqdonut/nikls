caf9ea64157381adafa01126d06b78c1b8c0fea3
Joel Kaasinen <joel.kaasinen@gmail.com>
2013-01-26 19:15:06 +0200

    adding transactions, showing optimized debt graph

--- Main.hs
+++ Main.hs
@@ -13,9 +13,10 @@ import Control.Monad.Trans (liftIO)
 import qualified Data.Map.Strict as M
 import Data.Text (Text)
 import Data.Text.Lazy (unpack)
-import Happstack.Server
+import Data.Time.Clock (getCurrentTime)
+import Happstack.Server hiding (method)
 import Text.Blaze.Html5 (Html, (!), a, form, input, p, toHtml, label)
-import Text.Blaze.Html5.Attributes (href)
+import Text.Blaze.Html5.Attributes (href, type_, name, size, action, method)
 import qualified Text.Blaze.Html5 as H
 
 main :: IO ()
@@ -25,7 +26,8 @@ main = bracket openDatabase
 
 route :: DB -> ServerPart Response
 route db = msum
-  [ dir "transactions" $ page_transactions db
+  [ dirs "transaction/list" $ page_transaction_list db
+  , dirs "transaction/add" $ page_transaction_add db
   , homePage db]
 
 template :: Text -> Html -> Response
@@ -40,21 +42,55 @@ template title body = toResponse $
 homePage db = do
   g <- liftIO $ viewDebtGraph db
   let bal = unBalances $ balances g
+  let pro = propagateDebts g
+  let people = M.keys g
   ok $ template "Home Page" $ do
     H.h1 "Balances"
-    H.ul $ forM_ (M.assocs $ g) $ \(p,status) -> H.li $ do
+    H.ul $ forM_ people $ \p -> H.li $ do
       toHtml p
       toHtml (" : " :: String)
       toHtml (M.findWithDefault (Balance 0) p bal)
-      H.ul $ forM_ (M.assocs $ status) $ \(p',balance) -> H.li $ do
+      H.ul $ H.li $ forM_ (M.assocs $ M.findWithDefault M.empty p g) $ \(p',balance) -> do
         toHtml p'
         toHtml (" : " :: String)
         toHtml balance
-    H.p $ a ! href "/transactions" $ "transactions"
-    H.p $ a ! href "/debtgraph" $ "debtgraph"
+        toHtml (", " :: String)
+      H.ul $ H.li $ forM_ (M.assocs $ M.findWithDefault M.empty p pro) $ \(p',balance) -> do
+        toHtml p'
+        toHtml (" : " :: String)
+        toHtml balance
+        toHtml (", " :: String)
 
-page_transactions :: DB -> ServerPart Response
-page_transactions db = do
+    transaction_add_form
+
+    H.p $ a ! href "/transaction/list" $ "transactions"
+
+transaction_add_form :: Html
+transaction_add_form = do
+  H.form ! method "GET" ! action "/transaction/add" $ do
+    H.p $ do H.label "Payer: " >> H.input ! type_ "text" ! name "payer" ! size "10"
+             H.label "Benefitors: " >> H.input ! type_ "text" ! name "benefitors" ! size "40"
+             H.label "Sum: " >> H.input ! type_ "text" ! name "sum" ! size "5"
+    H.p $ H.label "Description: " >> H.input ! type_ "text" ! name "description" ! size "40"
+    H.p $ H.input ! type_ "submit" ! name "add"
+
+page_transaction_add :: DB -> ServerPart Response
+page_transaction_add db = do
+  payer <- look "payer"
+  benefitors_ <- look "benefitors"
+  let benefitors = words benefitors_
+  sum_ <- look "sum"
+  let sum = read sum_ :: Int
+  description <- look "description"
+  time <- liftIO getCurrentTime
+  let st = SimpleTransaction (Person payer) (map Person benefitors)
+           (Balance sum) description time
+  liftIO $ addTransaction db st
+  ok $ template "Transaction added!" $
+    toHtml st
+
+page_transaction_list :: DB -> ServerPart Response
+page_transaction_list db = do
   ts <- liftIO $ viewTransactions db
   ok $ template "Transactions" $ do
     H.ul $ forM_ ts (H.li . toHtml)
