a19ddf5d8d02d766539e44107db18c25ab799055
Joel Kaasinen <joel.kaasinen@gmail.com>
2016-05-04 12:38:45 +0300

    simple IORef database

--- src/Main.hs
+++ src/Main.hs
@@ -5,27 +5,41 @@ module Main where
 import Api
 import Model
 
+import Data.IORef
+import Data.Monoid
+import Control.Applicative
+import Control.Monad.IO.Class
 import Servant
 import Network.Wai
 import Network.Wai.Handler.Warp
 import qualified Data.Map.Strict as M
 
-server :: Server Api
-server = account :<|> transaction :<|> allTransactions
+type Database = IORef [Transaction]
+
+dbState :: MonadIO m => Database -> m Transaction
+dbState db = liftIO $ mconcat <$> readIORef db
+
+dbTransactions :: MonadIO m => Database -> m [Transaction]
+dbTransactions db = liftIO $ readIORef db
+
+server :: Database -> Server Api
+server db = account :<|> transaction :<|> allTransactions
   where account :: Account -> Handler Balance :<|> Handler [Transaction]
         account acc = balance acc :<|> transactionsFor acc
         balance :: Account -> Handler Balance
-        balance acc = return $ transactionBalances state M.! acc
+        balance acc = do state <- dbState db
+                         return $ transactionBalances state M.! acc
         transactionsFor :: Account -> Handler [Transaction]
-        transactionsFor acc = return $ filter (concerns acc) database
+        transactionsFor acc = filter (concerns acc) <$> dbTransactions db
         transaction :: Timestamp -> Handler Transaction
         transaction ts =
-          return $ head $ filter (\t -> transactionTime t == ts) database
+          head . filter (\t -> transactionTime t == ts) <$> dbTransactions db
         allTransactions :: Handler [Transaction]
-        allTransactions = return database
+        allTransactions = dbTransactions db
 
-app :: Application
-app = serve Api.api server
+app :: Database -> Application
+app db = serve Api.api (server db)
 
 main :: IO ()
-main = run 8081 app
+main = do db <- newIORef database
+          run 8081 (app db)
