e36bdc4aa97b4e3047c10c162263568282ea2ff7
Joel Kaasinen <joel.kaasinen@nitor.fi>
2016-12-09 18:15:03 +0200

    some actual tests

--- test/Test.hs
+++ test/Test.hs
@@ -1,11 +1,29 @@
+{-# LANGUAGE StandaloneDeriving, GeneralizedNewtypeDeriving #-}
+
 module Main where
 
+import Model
+
 import Test.QuickCheck
+import Test.QuickCheck.Property.Common
+import Test.QuickCheck.Property.Monoid
 import Test.Framework
 import Test.Framework.Providers.QuickCheck2
 
-prop_True = True
+-- Model --
 
-tests = [ testProperty "True" prop_True ]
+deriving instance Arbitrary Sum
+
+prop_Sum_Monoid = eq $ prop_Monoid (T :: T Sum)
+
+deriving instance Arbitrary Account
+deriving instance Arbitrary Balances
+
+prop_Balances_Monoid = eq $ prop_Monoid (T :: T Sum)
+
+-- Running --
+
+tests = [ testProperty "Sum_Monoid" prop_Sum_Monoid
+        , testProperty "Balances_Monoid" prop_Balances_Monoid ]
 
 main = defaultMain tests
